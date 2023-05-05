d008895e5447539c7c99b6b8a5d19d41085b36ed
Luis Sánchez <wichis6661@gmail.com>
2023-05-05 06:54:35 -0600

    db mod

--- db-conf/db.js
+++ db-conf/db.js
@@ -1,3 +1,5 @@
+npm install bcryptjs
+
 use eCommerce
 
 db.createCollection('roles')
@@ -15,6 +17,88 @@ db.roles.insertMany([
 ])
 
 db.createCollection('usuarios')
+
+const bcrypt = require('bcryptjs');
+db.usuarios.insertMany([
+    
+    {
+        'nombre': 'common1',
+        'password': bcrypt.hashSync('123456', bcrypt.genSaltSync()),
+        'rol': 'COMMON_ROLE',
+        'estado': true
+    },
+    {
+        'nombre': 'common2',
+        'password': bcrypt.hashSync('123456', bcrypt.genSaltSync()),
+        'rol': 'COMMON_ROLE',
+        'estado': true
+    },
+    {
+        'nombre': 'common3',
+        'password': bcrypt.hashSync('123456', bcrypt.genSaltSync()),
+        'rol': 'COMMON_ROLE',
+        'estado': true
+    },
+    {
+        'nombre': 'common4',
+        'password': bcrypt.hashSync('123456', bcrypt.genSaltSync()),
+        'rol': 'COMMON_ROLE',
+        'estado': true
+    },
+    {
+        'nombre': 'common5',
+        'password': bcrypt.hashSync('123456', bcrypt.genSaltSync()),
+        'rol': 'COMMON_ROLE',
+        'estado': true
+    },
+    {
+        'nombre': 'package1',
+        'password': bcrypt.hashSync('123456', bcrypt.genSaltSync()),
+        'rol': 'PACKAGE_ROLE',
+        'estado': true
+    },
+    {
+        'nombre': 'package2',
+        'password': bcrypt.hashSync('123456', bcrypt.genSaltSync()),
+        'rol': 'PACKAGE_ROLE',
+        'estado': true
+    },
+    {
+        'nombre': 'admin1',
+        'password': bcrypt.hashSync('123456', bcrypt.genSaltSync()),
+        'rol': 'ADMIN_ROLE',
+        'estado': true
+    }
+])
+
+db.createCollection('categorias')
+
+db.categorias.insertMany([
+    {
+        'nombre': 'Tecnología',
+        'estado': true
+    },
+    {
+        'nombre': 'Hogar',
+        'estado': true
+    },
+    {
+        'nombre': 'Académico',
+        'estado': true
+    },
+    {
+        'nombre': 'Literatura',
+        'estado': true
+    },
+    {
+        'nombre': 'Decoración',
+        'estado': true
+    },
+    {
+        'nombre': 'Otros',
+        'estado': true
+    }
+])
 
 db.createCollection('tarjetas')
 
